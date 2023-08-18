5870438bfab364e741fd1ccc74f9c9c58aa0ca12
Souvik Das <136602066+souda0303@users.noreply.github.com>
2023-08-18 13:41:15 +0530

    commit done

---
+++ script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll(".card-buttons button");
+const sections = document.querySelectorAll(".card-section");
+const card = document.querySelector(".card");
+var mainContent = document.getElementById("mainContent");
+var intro = document.querySelector(".intro");
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  setTimeout(function() {
+      intro.style.opacity = "0";
+      mainContent.style.opacity = "1";
+  }, 5000);
+});
+
+
+const handleButtonClick = e => {
+  const targetSection = e.target.getAttribute("data-section");
+  const section = document.querySelector(targetSection);
+  targetSection !== "#about" ?
+  card.classList.add("is-active") :
+  card.classList.remove("is-active");
+  card.setAttribute("data-state", targetSection);
+  sections.forEach(s => s.classList.remove("is-active"));
+  buttons.forEach(b => b.classList.remove("is-active"));
+  e.target.classList.add("is-active");
+  section.classList.add("is-active");
+};
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", handleButtonClick);
+});
+
